80dc6352a9c979fbbf7d2beb712f74829a5f9005
Derek <derek84worth@gmail.com>
2019-01-12 16:35:53 -0600

    Class Dates
    Added class dates to summary report

--- src/ucv/Main.java
+++ src/ucv/Main.java
@@ -39,15 +39,19 @@ public class Main {
                 String line = br.readLine();
                 String[] tokens = line.split(",");
                 if(tokens.length == 7 && tokens[0].contains("Class Number") && tokens[4].contains("Class Number")) {
-                    UCTClass c = new UCTClass("unassigned", "unassigned");
+                    UCTClass c = new UCTClass("unassigned","--","--","unassigned","--","--");
                     classList.add(c);
                     classMap.put("unassigned", c);
                     while((line = br.readLine()) != null) {
                         tokens = line.split(",");
-                        String p1 = tokens[0];
-                        String p2 = tokens[4];
+                        String p1      = tokens[0];
+                        String p1Start = tokens[1];
+                        String p1End   = tokens[2];
+                        String p2      = tokens[4];
+                        String p2Start = tokens[5];
+                        String p2End   = tokens[6];
                         // create class
-                        c = new UCTClass(p1, p2);
+                        c = new UCTClass(p1, p1Start, p1End, p2, p2Start, p2End);
                         // add to maps
                         classList.add(c);
                         classMap.put(p1, c);
@@ -197,15 +201,20 @@ public class Main {
     }
     
     public static class UCTClass {
-        String p1, p2;
+        String p1, p2, p1Start, p1End, p2Start, p2End;
         LinkedList<Student> studList;
         HashMap<String, Student> studMap;
         
-        public UCTClass(String p1, String p2) {
+        public UCTClass(String p1, String p1Start, String p1End, String p2, String p2Start, String p2End) {
             studList = new LinkedList();
             studMap = new HashMap();
             this.p1 = p1;
+            this.p1Start = p1Start;
+            this.p1End = p1End;
             this.p2 = p2;
+            this.p2Start = p2Start;
+            this.p2End = p2End;
+            
         }
         
         public void addStudent(Student stud) {
@@ -228,7 +237,7 @@ public class Main {
             if(p1.equals("unassigned")) {
                 return ": unassigned";
             } else {
-                return ": P1 " + p1.substring(15) + " | P2 " + p2.substring(15);
+                return ": P1 " + p1.substring(15) + " (" + p1Start + " - " + p1End + ") | P2 " + p2.substring(15) + " (" + p2Start + " - " + p2End + ")";
             }
         }
     }
